1c9dced197408ab684070c28fb45845955bdd500
Andrzej Rybczak <andrzej@rybczak.net>
2021-06-24 02:12:36 +0200

    Add the STME effect

---
+++ src/Effectful/STM.hs
@@ -0,0 +1,20 @@
+module Effectful.STM
+  ( STME
+  , runSTME
+  , atomically
+  ) where
+
+import qualified Control.Monad.STM as S
+
+import Effectful.Internal.Effect
+import Effectful.Internal.Monad
+
+-- | Run 'S.STM' computations using the @stm@ library.
+data STME :: Effect where
+  STME :: STME m r
+
+runSTME :: IOE :> es => Eff (STME : es) a -> Eff es a
+runSTME = evalEffect (IdE STME)
+
+atomically :: STME :> es => S.STM a -> Eff es a
+atomically = unsafeEff_ . S.atomically

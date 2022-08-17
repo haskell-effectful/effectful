16ff366d72a6e02defb41814dfb86726e8227478
Andrzej Rybczak <andrzej@rybczak.net>
2022-08-17 17:49:42 +0200

    Test purely dynamic Reader

--- effectful/tests/ReaderTests.hs
+++ effectful/tests/ReaderTests.hs
@@ -5,12 +5,13 @@ import Test.Tasty.HUnit
 
 import Effectful
 import Effectful.Dispatch.Dynamic
-import Effectful.Reader.Static
+import Effectful.Reader.Dynamic
 import qualified Utils as U
 
 readerTests :: TestTree
 readerTests = testGroup "Reader"
-  [ testCase "local works in handlers" test_localInHandler
+  [ testCase "local works in handlers (dynamic/static)" $ test_localInHandler runReader
+  , testCase "local works in handlers (dynamic/pure)" $ test_localInHandler runPureReader
   ]
 
 data SomeEff :: Effect where
@@ -18,9 +19,21 @@ data SomeEff :: Effect where
 
 type instance DispatchOf SomeEff = Dynamic
 
-test_localInHandler :: Assertion
-test_localInHandler = runEff . runReader "global" . interpret f $ do
+test_localInHandler
+  :: (forall r es a. r -> Eff (Reader r : es) a -> Eff es a)
+  -> Assertion
+test_localInHandler runR = runEff . runR "global" . interpret f $ do
   local (const "local") $ send SomeAction
   where
     f :: [IOE, Reader String] :>> es => EffectHandler SomeEff es
     f _ SomeAction = U.assertEqual "expected result" "local" =<< ask
+
+-- | Purely dynamic Reader for testing purposes.
+runPureReader :: r -> Eff (Reader r : es) a -> Eff es a
+runPureReader r0 = interpret (handler r0)
+  where
+    handler :: r -> EffectHandler (Reader r) handlerEs
+    handler r env = \case
+      Ask       -> pure r
+      Local f m -> localSeqUnlift env $ \unlift -> do
+        unlift $ interpose (handler $ f r) m
